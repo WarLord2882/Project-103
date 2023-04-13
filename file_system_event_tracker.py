a4e5ceaa0f747ef25a245707efeb2ba865d66490
WarLord2287 <77100801+WarLord2882@users.noreply.github.com>
2023-04-13 11:44:14 +0530

    Add files via upload

---
+++ file_system_event_tracker.py
@@ -0,0 +1,51 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir="C:\\Users\\Admin\\Downloads"
+
+dir_tree={
+   "Image_Files":[".jpg",".png",".jpeg",".gif",".jfif"],
+   "Video_Files":[".mpg",".mp2",".mpeg",".mpe",".mp4",".m4p",".mov",".avi"],
+   "Document_Files":[".ppt",".pdf",".docx",".xls",".xlsx",".txt"]
+
+}
+
+#Event Handler Class
+class FileMovementHandler(FileSystemEventHandler):
+    
+    def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created")
+
+    def on_deleted(self, event):
+        print(f"Oops! Someone deleted {event.src_path}!")
+
+    def on_modified(self, event):
+        print(f"{event.src_path} has been modified!")
+
+    def on_moved(self, event):
+        print(f"{event.src_path} has been moved!")            
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+# Start the Observer
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
